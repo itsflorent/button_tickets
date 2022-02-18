64c79718e4b422466c7cae8bc50d92f9e18312fd
its_florent <70605487+itsflorent@users.noreply.github.com>
2022-02-18 19:20:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ui import Button, View
-from discord.commands import commands
-import asyncio
-bot = commands.Bot()
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 GUILD_ID = 123
 TEAM_ROLE = 123
@@ -10,9 +10,13 @@ TICKET_CHANNEL = 123
 CATEGORY_ID = 123
 
 
+@bot.event
+async def on_ready():
+    print("Online!")
+
 @bot.command()
-async def ticket_msg(ctx):
-    button1 = Button(label="Open a ticket", style=discord.ButtonStyle.blurple, custom_id="ticket_button")
+async def ticketmsg(ctx):
+    button1 = Button(label="Ticket opened!", style=discord.ButtonStyle.blurple, custom_id="ticket_button")
     view = View()
     view.add_item(button1)
     embed = discord.Embed(description=f"Open a ticket via. Button", title=f"Ticket System")
@@ -29,22 +33,22 @@ async def on_interaction(interaction):
         if "ticket_button" in str(interaction.data):
             guild = bot.get_guild(GUILD_ID)
             for ticket in guild.channels:
-                if str(interaction.author.id) in ticket.name:
+                if str(interaction.user.id) in ticket.name:
                     embed = discord.Embed(description=f"You can have only one ticket open at a time!")
                     await interaction.response.send_message(embed=embed, ephemeral=True)
                     return
 
             category = bot.get_channel(CATEGORY_ID)
-            ticket_channel = await guild.create_text_channel(f"ticket-{interaction.author.id}", category=category,
-                                                            topic=f"Ticket von {interaction.author} \nClient-ID: {interaction.author.id}")
+            ticket_channel = await guild.create_text_channel(f"ticket-{interaction.user.id}", category=category,
+                                                            topic=f"Ticket von {interaction.user} \nClient-ID: {interaction.user.id}")
 
             
             await ticket_channel.set_permissions(guild.get_role(TEAM_ROLE), send_messages=False,
                                                 read_messages=False)
-            await ticket_channel.set_permissions(interaction.author, send_messages=True, read_messages=True, add_reactions=False,
+            await ticket_channel.set_permissions(interaction.user, send_messages=True, read_messages=True, add_reactions=False,
                                                 embed_links=True, attach_files=True, read_message_history=True,
                                                 external_emojis=True)
-            embed = discord.Embed(description=f'Welcome in your Ticket {interaction.author.mention}.\n'
+            embed = discord.Embed(description=f'Welcome in your Ticket {interaction.user.mention}.\n'
                                             f'On of our staff will be help soon as possible!\n'
                                             f'If you want to close the ticket write `!close` in this channel.',
                                 color=62719)
